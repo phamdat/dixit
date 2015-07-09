05b636b11a2c599ca97dd41ca297c4dcc84a36c4
ttdat89 <ttdat89@gmail.com>
2015-07-09 23:35:06 +0700

    . add easy touch

--- Dixit-frontend/Assets/Scripts/Controllers/GameController.cs
+++ Dixit-frontend/Assets/Scripts/Controllers/GameController.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
+    public GameObject _mainBoard;
+    public GameObject _imageItem;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
